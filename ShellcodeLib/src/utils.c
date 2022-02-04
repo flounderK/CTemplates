7880255790fb59ef7821588f3841e1de4c2731f8
Clif Wolfe <clifwolfe1@gmail.com>
2022-02-04 17:34:48 -0500

    fixed broken print function

--- ShellcodeLib/src/utils.c
+++ ShellcodeLib/src/utils.c
@@ -3,14 +3,18 @@
 #include "_syscall.h"
 
 void print_hex_size_t(size_t val){
-    unsigned long i;
-    unsigned char hex[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+    long long i;
+    unsigned char hex[] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
                      0x37, 0x38, 0x39,
                      0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
                      0x0};
 
-    for (i = ((sizeof(val)*8)-4); i > 0; i -= 4UL){
-        _write(1, &hex[((val >> i) & 0xf)], 1);
+    for (i = ((sizeof(val)*8)-8); i >= 0; i = i - 8){
+        _write(1, &hex[((val >> (i+4)) & 0xful)], 1);
+        //_write(1, "\n", 1);
+        _write(1, &hex[((val >> i) & 0xful)], 1);
+        //_write(1, "\n", 1);
+        //_write(1, "\n", 1);
     }
     _write(1, "\n", 1);
 }

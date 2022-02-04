3672913a650467ca856bf72d105f713d0ffaecbb
Clif Wolfe <clifwolfe1@gmail.com>
2022-02-04 17:35:11 -0500

    Migrated to clang

--- ShellcodeLib/src/shellcode.c
+++ ShellcodeLib/src/shellcode.c
@@ -6,15 +6,11 @@
 #include "utils.h"
 
 
-// redefine syscalls to avoid libc's wrappers
-
 //Make sure to use static const arrays if you use any arrays, otherwise gcc might decide to add a
 //call to memcpy
 
 int _start(void)
 {
-    void* load_addr = get_load_address();
-    //char buf[64] = {0};
     _write(1, "Hello World\n", 12);
 
     return 0;

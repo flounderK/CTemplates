2cd91032090e16354cc3bd8145850976ab5ab1c9
Clif Wolfe <clifwolfe1@gmail.com>
2022-07-21 14:35:30 -0400

    added a few useful commented out options and included cpp compiler setting

--- CMakeTemplate/cmake/clang_x86_64.cmake
+++ CMakeTemplate/cmake/clang_x86_64.cmake
@@ -1,6 +1,8 @@
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
 set(triple x86_64-pc-linux-gnu)
 set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
